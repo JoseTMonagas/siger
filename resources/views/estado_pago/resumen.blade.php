6a0fd5e8e6829137ae32801df30e9d54dc9a5904
Jose Monagas <jose_tadeomong@hotmail.com>
2021-12-07 11:19:57 -0300

    fix: Both LIQUIDADO buttons showing at once

--- resources/views/estado_pago/resumen.blade.php
+++ resources/views/estado_pago/resumen.blade.php
@@ -132,9 +132,10 @@
                                     @isset($guia->liquidado)
                                     <button @if($isLogistica) disabled @endif class="btn btn-info">LIQUIDADO</button>
                                     @else
-                                    @endisset
                                     <button @if($isLogistica) disabled @endif type="submit" class="btn btn-warning">NO LIQUIDADO</button>
+                                    @endisset
                                 </form>
+                                <a href="{{ route("estado_pago_nota_credito", $guia) }}" target="_blank">Ver NC Proforma</a>
                             </td>
                         </tr>
                         @endforeach
